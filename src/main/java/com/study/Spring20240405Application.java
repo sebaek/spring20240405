1857a116f777ab466e9b1cd1971f74b8b0700860
sebaek <sebaek@gmail.com>
2024-04-05 11:38:30 +0900

    first commit

---
+++ src/main/java/com/study/Spring20240405Application.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20240405Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring20240405Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/Spring20240405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20240405ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
