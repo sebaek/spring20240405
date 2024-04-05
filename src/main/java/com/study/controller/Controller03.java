197ec549e180b32959213e2fbbf56c463f365f8e
sebaek <sebaek@gmail.com>
2024-04-05 15:18:13 +0900

    요청 경로와 파라미터

--- src/main/java/com/study/controller/Controller03.java
+++ src/main/java/com/study/controller/Controller03.java
@@ -13,5 +13,33 @@ public class Controller03 {
         System.out.println("Controller03.method1");
     }
 
-    // 경로 : /main3/sub2 
+    // 경로 : /main3/sub2
+    @RequestMapping("sub2")
+    public void method2() {
+        System.out.println("Controller03.method2");
+    }
+
+    // 경로 : /main3/sub3
+    // 파라미터 : name
+    // /main3/sub3?name
+    @RequestMapping(value = "sub3", params = "name")
+    public void method3() {
+        System.out.println("Controller03.method3");
+    }
+
+    // /main3/sub3?name&age
+    @RequestMapping(value = "sub3", params = {"name", "age"})
+    public void method4() {
+        System.out.println("Controller03.method4");
+    }
+
+    // /main3/sub3?address&city
+    // todo: method5 작성
+
+
+    // todo: 아래 메소드 실행될 수 있는 경로 작성
+    @RequestMapping(value = "sub3", params = {"country", "city"})
+    public void method6() {
+        System.out.println("Controller03.method6");
+    }
 }
