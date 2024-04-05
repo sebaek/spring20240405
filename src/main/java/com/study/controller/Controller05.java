f62e7010304d695efac80676a06e3f9d3bf035be
sebaek <sebaek@gmail.com>
2024-04-05 16:40:48 +0900

    request param

--- src/main/java/com/study/controller/Controller05.java
+++ src/main/java/com/study/controller/Controller05.java
@@ -20,5 +20,35 @@ public class Controller05 {
     // /main5/sub2?address=hongdae
 
     // "~ 로 가는 경로 탐색"
+    @RequestMapping("sub2")
+    public void method2(@RequestParam("address") String param) {
+        System.out.println(param + "으로 가는 경로 탐색");
+    }
 
+    // /main5/sub3?age=13
+    // /main5/sub3?age=20
+    // /main5/sub3?age=40
+    // /main5/sub3?age=70
+    @RequestMapping("sub3")
+    public void method3(@RequestParam("age") Integer param) {
+        if (param < 18) {
+            System.out.println("청소년 관련 정보 출력");
+        } else if (param < 35) {
+            System.out.println("청년 관련 정보 출력");
+        } else if (param < 65) {
+            System.out.println("중장년 관련 정보 출력");
+        } else {
+            System.out.println("노인 관련 정보 출력");
+        }
+    }
+
+    // /main5/sub4?height=145.1
+    // /main5/sub4?height=190.5
+
+    // 0~145.0 작은 키
+    // 145.0 ~ 190.0 중간 키
+    // 190 ~ 큰 키
+
+    // todo: height 요청 파라미터를 double 타입으로 활요하는 메소드 작성
+    
 }
