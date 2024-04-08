d464fd14b1165b392a40bdb15681222d85f20116
sebaek <sebaek@gmail.com>
2024-04-08 10:20:05 +0900

    request paramter

--- src/main/java/com/study/controller/Controller06.java
+++ src/main/java/com/study/controller/Controller06.java
@@ -29,5 +29,26 @@ public class Controller06 {
     // /main6/sub3?name=lee
     // /main6/sub3?age=55
     // todo: method3이 실행되도록 작성
+    @RequestMapping("sub3")
+    public void method3(@RequestParam(value = "name", required = false) String param1,
+                        @RequestParam(value = "age", required = false) Integer param2) {
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+    // /main6/sub4?name=손흥민&age=33
+    // /main6/sub4?name=이강인
+    // /main6/sub4?age=55
+    @RequestMapping("sub4")
+    public void method4(@RequestParam(required = false) String name,
+                        @RequestParam(required = false) Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+
+    // /main6/sub5?address=shinchon&city=seoul
+    // /main6/sub5?city=busan
+    // /main6/sub5?address=강남
+    // todo: method5 작성
 
 }
