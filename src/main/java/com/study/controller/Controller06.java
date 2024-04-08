c024b6900acf4fbdb96bfd3570612c08772bc539
sebaek <sebaek@gmail.com>
2024-04-08 11:08:24 +0900

    request paramter

--- src/main/java/com/study/controller/Controller06.java
+++ src/main/java/com/study/controller/Controller06.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("main6")
 public class Controller06 {
@@ -72,5 +74,30 @@ public class Controller06 {
     // /main6/sub7?name=강인
     // /main6/sub7
     // todo : method7 작성
+
+    // 메소드 아규먼트 타입이 기본타입(wrapper포함), String (의 배열) 이면
+    // @RequestParam으로 간주됨
+
+    @RequestMapping("sub7")
+    public void method7(String name, Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+
+    // /main6/sub8?name=흥민&address=런던&address=서울
+    // /main6/sub8?name=흥민&address=런던
+    // /main6/sub8?name=흥민
+    // /main6/sub8
+    @RequestMapping("sub8")
+    public void method8(String name, String[] address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + Arrays.toString(address));
+    }
+
+    // /main6/sub9?age=33
+    // /main6/sub9?age=44&age=55
+    // /main6/sub9?city=서울
+    // /main6/sub9?city=서울&city=부산
+    // todo: method9 작성
 }
 
