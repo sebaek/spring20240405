767bade1b0cdf10700a964b91cde4d259d9e442d
sebaek <sebaek@gmail.com>
2024-04-08 10:32:39 +0900

    request parameter binding

--- src/main/java/com/study/controller/Controller06.java
+++ src/main/java/com/study/controller/Controller06.java
@@ -50,5 +50,27 @@ public class Controller06 {
     // /main6/sub5?city=busan
     // /main6/sub5?address=강남
     // todo: method5 작성
+    @RequestMapping("sub5")
+    public void method5(@RequestParam(required = false) String address,
+                        @RequestParam(required = false) String city) {
+        System.out.println("address = " + address);
+        System.out.println("city = " + city);
+    }
 
+    // /main6/sub6?address=강남&city=서울
+    // /main6/sub6?address=신촌
+    // /main6/sub6?city=부산
+    @RequestMapping("sub6")
+    public void method6(String address, // required 는 false
+                        String city) {
+        System.out.println("address = " + address);
+        System.out.println("city = " + city);
+    }
+
+    // /main6/sub7?name=흥민&age=77
+    // /main6/sub7?age=66
+    // /main6/sub7?name=강인
+    // /main6/sub7
+    // todo : method7 작성
 }
+
