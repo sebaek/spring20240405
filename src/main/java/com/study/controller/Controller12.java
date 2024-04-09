cb75d0fd66d38335f3239f055191042a4e2801c5
sebaek <sebaek@gmail.com>
2024-04-09 15:45:14 +0900

    if

--- src/main/java/com/study/controller/Controller12.java
+++ src/main/java/com/study/controller/Controller12.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("main12")
@@ -15,6 +16,22 @@ public class Controller12 {
         model.addAttribute("age", 15);
         model.addAttribute("products", List.of());
 //        model.addAttribute("products", List.of("Apple", "Banana"));
+
+        model.addAttribute("member",
+                Map.of("country", "korea",
+                        "age", 30));
+
+//        model.addAttribute("member",
+//                Map.of("country", "korea",
+//                        "age", 15));
+//
+//        model.addAttribute("member",
+//                Map.of("country", "us",
+//                        "age", 30));
+//
+//        model.addAttribute("member",
+//                Map.of("country", "us",
+//                        "age", 15));
         // /main12/sub1.jsp
     }
 }
