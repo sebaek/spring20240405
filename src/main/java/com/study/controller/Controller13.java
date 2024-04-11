639be455d3a7fa42f7af360ea6a04dc5e2127731
sebaek <sebaek@gmail.com>
2024-04-11 11:38:17 +0900

    기타 태그들

---
+++ src/main/java/com/study/controller/Controller13.java
@@ -0,0 +1,13 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main13")
+public class Controller13 {
+    @RequestMapping("sub1")
+    public void sub1() {
+
+    }
+}
