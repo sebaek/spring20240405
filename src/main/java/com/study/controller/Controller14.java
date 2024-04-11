6adbaccdda2deeeec42b01858175e1f731f98861
sebaek <sebaek@gmail.com>
2024-04-11 15:03:26 +0900

    model attribute name

--- src/main/java/com/study/controller/Controller14.java
+++ src/main/java/com/study/controller/Controller14.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("main14")
 public class Controller14 {
@@ -26,12 +28,14 @@ public class Controller14 {
     public void sub2(Model model) {
         MyBean142 o1 = new MyBean142();
         o1.setName("김민재");
+        MyBean142 o2 = new MyBean142();
+        o2.setName("이정후");
 
         // attribute 명은
         // 클래스명을 lowerCamelCase 로 변경된 이름으로 결정
-        // myBean142
         // 좋은 습관은 이름을 직접 작성하는 것
-        model.addAttribute(o1);
+        model.addAttribute(o1); // myBean142
+        model.addAttribute(List.of(o1, o2)); // myBean142List
 
     }
 
--- src/main/webapp/WEB-INF/view/main14/sub2.jsp
+++ src/main/webapp/WEB-INF/view/main14/sub2.jsp
@@ -6,5 +6,8 @@
 </head>
 <body>
 <p>${myBean142.name}</p>
+<hr>
+<p>${myBean142List[0].name}</p>
+<p>${myBean142List[1].name}</p>
 </body>
 </html>
