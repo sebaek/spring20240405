e3c6549857f8d7821157c744c95d632dd5221c5f
sebaek <sebaek@gmail.com>
2024-04-16 12:46:06 +0900

    sql

---
+++ sql/01sql.sql
@@ -0,0 +1,13 @@
+-- SQL 주석
+# SQL 주석
+
+# 명령문 실행 ctrl + enter
+SELECT *
+FROM w3schools.Employees;
+
+# 명령문 종료는 ; 꼭 사용
+SELECT *
+FROM w3schools.Customers;
+
+# table : 데이터들이 저장되는 공간
+#  행, 열로 이루어져 있음
