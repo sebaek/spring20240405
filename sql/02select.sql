d0e28551539f2a2b2e256c1f27a30221d3c9cf20
sebaek <sebaek@gmail.com>
2024-04-16 14:25:56 +0900

    select

---
+++ sql/02select.sql
@@ -0,0 +1,37 @@
+# 데이터베이스 선택
+USE w3schools;
+
+# 데이터 조회
+# SELECT * FROM 테이블명;
+SELECT *
+FROM Customers;
+
+SELECT *
+FROM Employees;
+
+# todo: Suppliers 테이블 조회
+SELECT *
+FROM Suppliers;
+
+# 특정 컬럼만 조회
+# SELECT 컬럼명, 컬럼명 FROM 테이블명
+SELECT CustomerName, City
+FROM Customers;
+
+# 모든 컬럼 조회
+# SELECT * FROM 테이블명
+SELECT *
+FROM Customers;
+
+# 테이블의 컬럼들 조회
+DESCRIBE Customers;
+DESC Customers;
+
+# todo : Employees 테이블의 컬럼이 어떤 것들이 있는 지 조회
+# todo : Employees 테이블 조회 (first name, last name)
+
+
+
+
+
+
