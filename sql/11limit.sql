94b875d9bf94cdcbbeb04c78f26a68e0ea927a8c
sebaek <sebaek@gmail.com>
2024-04-18 16:08:00 +0900

    limit

--- sql/11limit.sql
+++ sql/11limit.sql
@@ -25,7 +25,25 @@ ORDER BY Price
 LIMIT 3;
 
 # todo : 최초로 주문된 주문 1개
+SELECT *
+FROM Orders
+ORDER BY OrderDate
+LIMIT 1;
+
 # todo : 가장 최근에 주문된 주문 5개
+SELECT *
+FROM Orders
+ORDER BY OrderDate DESC
+LIMIT 5;
 # todo : 가장 나이가 많은 직원 1명
+SELECT *
+FROM Employees
+ORDER BY BirthDate
+LIMIT 1;
 # todo : CategoryId 가 3 인 상품 중 가장 비싼 상품 2개
+SELECT *
+FROM Products
+WHERE CategoryID = 3
+ORDER BY Price DESC
+LIMIT 2;
 
