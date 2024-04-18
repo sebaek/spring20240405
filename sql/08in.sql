e6f0b3d2ecf0d5d6c28eee87fc14ab5529ad5457
sebaek <sebaek@gmail.com>
2024-04-18 11:05:28 +0900

    in

---
+++ sql/08in.sql
@@ -0,0 +1,20 @@
+#08in.sql
+USE w3schools;
+
+# IN 하나라도 같으면...
+SELECT *
+FROM Customers
+WHERE Country = 'Germany'
+   OR Country = 'UK';
+
+SELECT *
+FROM Customers
+WHERE Country IN ('germany', 'uk');
+
+SELECT *
+FROM Customers
+WHERE Country IN ('germany', 'uk', 'sweden', 'france');
+
+# todo : 2, 3, 6 번 카테고리에 있는 상품들 조회
+# todo : 브라질, 일본, 이탈리아에 있는 공급자(Suppliers) 조회
+
