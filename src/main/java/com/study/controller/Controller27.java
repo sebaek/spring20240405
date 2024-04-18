500adb57fb47e0558c5e5bd29c63351a89ffc9fb
sebaek <sebaek@gmail.com>
2024-04-18 16:31:52 +0900

    paging

---
+++ src/main/java/com/study/controller/Controller27.java
@@ -0,0 +1,60 @@
+package com.study.controller;
+
+import com.study.domain.MyBean254Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("main27")
+public class Controller27 {
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("sub1")
+    public String sub1(Integer page, Model model) throws SQLException {
+        int offset = (page - 1) * 10;
+        String sql = """
+                SELECT * 
+                FROM Customers
+                ORDER BY CustomerID
+                LIMIT ?, 10
+                """;
+
+        var list = new ArrayList<MyBean254Customer>();
+        
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, offset);
+        ResultSet rs = ps.executeQuery();
+
+        try (rs; ps; conn) {
+            while (rs.next()) {
+                MyBean254Customer customer = new MyBean254Customer();
+
+                customer.setId(rs.getInt(1));
+                customer.setName(rs.getString(2));
+                customer.setContactName(rs.getString(3));
+                customer.setAddress(rs.getString(4));
+                customer.setCity(rs.getString(5));
+                customer.setPostalCode(rs.getString(6));
+                customer.setCountry(rs.getString(7));
+
+                list.add(customer);
+            }
+
+            model.addAttribute("customerList", list);
+        }
+
+        return "main27/sub1";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main27/sub1.jsp
@@ -0,0 +1,45 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <style>
+        table, tr, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        table {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+<hr>
+<table>
+    <thead>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>contact</th>
+        <th>address</th>
+        <th>city</th>
+        <th>post</th>
+        <th>country</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${customerList}" var="customer">
+        <tr>
+            <td>${customer.id}</td>
+            <td>${customer.name}</td>
+            <td>${customer.contactName}</td>
+            <td>${customer.address}</td>
+            <td>${customer.city}</td>
+            <td>${customer.postalCode}</td>
+            <td>${customer.country}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+</body>
+</html>
