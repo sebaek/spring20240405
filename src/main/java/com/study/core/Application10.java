e0c560ef171f492a9dc50146580ff7effbe34adf
sebaek <sebaek@gmail.com>
2024-04-23 10:36:33 +0900

    필드 주입

--- src/main/java/com/study/core/Application10.java
+++ src/main/java/com/study/core/Application10.java
@@ -2,8 +2,11 @@ package com.study.core;
 
 import lombok.Getter;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application10 {
@@ -14,13 +17,29 @@ public class Application10 {
         System.out.println("field = " + field); // null이 아니게
     }
 }
-// todo ; 적절한 annotation 작성
 
+// todo ; 적절한 annotation 작성
+@Configuration
+class MyConfiguration10 {
+    @Bean
+    public MyClass101 myClass101() {
+        return new MyClass101();
+    }
+
+    @Bean
+    public MyClass102 myClass102() {
+        return new MyClass102();
+    }
+}
+
+//@Component
 @Getter
 class MyClass101 {
+    @Autowired
     private MyClass102 field;
 }
 
+//@Component
 class MyClass102 {
 
 }
