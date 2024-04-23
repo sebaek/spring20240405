28ac66a1855d8ece6cba8226a90a9cfc2d5d2265
sebaek <sebaek@gmail.com>
2024-04-23 11:13:41 +0900

    setter 주입

---
+++ src/main/java/com/study/core/Applcation14.java
@@ -0,0 +1,32 @@
+package com.study.core;
+
+import lombok.Getter;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Applcation14 {
+    public static void main(String[] args) {
+        BeanFactory bf = SpringApplication.run(Applcation14.class, args);
+        MyClass141 b = bf.getBean(MyClass142.class).getField();
+        System.out.println("b = " + b);
+    }
+}
+
+@Component
+class MyClass141 {
+}
+
+@Component
+@Getter
+class MyClass142 {
+    private MyClass141 field;
+
+    @Autowired
+    public void setField(MyClass141 field) {
+        this.field = field;
+    }
+}
