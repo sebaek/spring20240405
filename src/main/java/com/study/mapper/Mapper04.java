7f19bef30560d7dbf49563dd8281e3ec001da074
sebaek <sebaek@gmail.com>
2024-04-24 14:09:02 +0900

    mybatis 설정 변경 ; snake_case to camelCase

--- src/main/java/com/study/mapper/Mapper04.java
+++ src/main/java/com/study/mapper/Mapper04.java
@@ -22,11 +22,7 @@ public interface Mapper04 {
     List<MyBean331> select1();
 
     @Select("""
-            SELECT string_col stringCol,
-                    int_col intCol,
-                    dec_col decCol,
-                    date_col dateCol,
-                    date_time_col dateTimeCol
+            SELECT *
             FROM my_table9
             """)
     List<MyBean332> select2();
