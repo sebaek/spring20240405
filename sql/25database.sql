c23c697444c67185be95f1b5663d11873c626763
sebaek <sebaek@gmail.com>
2024-04-25 14:05:40 +0900

    database

---
+++ sql/25database.sql
@@ -0,0 +1,21 @@
+# 25database.sql
+
+# 사용할 데이터베이스(스키마) 선택하기
+USE w3schools;
+
+SHOW tables;
+
+# 새 데이터베이스 만들기
+CREATE DATABASE mydb1;
+USE mydb1;
+SHOW tables;
+CREATE TABLE my_table1
+(
+    name VARCHAR(3),
+    age  INT
+);
+
+# todo ; mydb2 데이터베이스 만들고 옮겨서
+#  새 테이블 하나 만들기
+
+
