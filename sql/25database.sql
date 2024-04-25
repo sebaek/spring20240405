eeeecaee2f58b6d560fbb0197b57a1c4adef91cf
sebaek <sebaek@gmail.com>
2024-04-25 14:12:45 +0900

    database 만들고 삭제하기

--- sql/25database.sql
+++ sql/25database.sql
@@ -17,5 +17,21 @@ CREATE TABLE my_table1
 
 # todo ; mydb2 데이터베이스 만들고 옮겨서
 #  새 테이블 하나 만들기
+CREATE DATABASE mydb2;
+USE mydb2;
+SHOW tables;
+CREATE TABLE my_table1
+(
+    col1 VARCHAR(3)
+);
+SELECT *
+FROM my_table1;
+SELECT *
+FROM mydb1.my_table1;
 
+# 데이터베이스 삭제 (매우 주의!!)
+DROP DATABASE mydb2;
+
+# todo; mydb1 삭제
+DROP DATABASE mydb1;
 
