1491badc3c528b07a0ef39fdb18ccef9f2af2d06
sebaek <sebaek@gmail.com>
2024-04-26 14:06:07 +0900

    join

---
+++ sql/38join.sql
@@ -0,0 +1,20 @@
+# 38join.sql
+USE mydb2;
+CREATE TABLE table7
+(
+    col1 INT,
+    col2 VARCHAR(3)
+);
+
+CREATE TABLE table8
+(
+    cola INT,
+    colb VARCHAR(3)
+);
+
+CREATE TABLE table9
+(
+    col3 INT,
+    colc INT,
+    info VARCHAR(3)
+);
