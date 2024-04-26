e6d25440ea64b6fd17790c842a4ca6d09778d278
sebaek <sebaek@gmail.com>
2024-04-26 11:42:41 +0900

    join

--- sql/36join.sql
+++ sql/36join.sql
@@ -18,3 +18,11 @@ WHERE p.ProductID = 1;
 
 # 10번 상품의 이름, 공급자 이름, 공급자의 국가 조회
 # Products, Suppliers
+SELECT p.ProductName, s.SupplierName, s.Country
+FROM Products p
+         JOIN Suppliers s
+              ON p.SupplierID = s.SupplierID
+WHERE p.ProductID = 10;
+
+# 10249번 주문(Orders)을 한 고객의 이름(Customers) 조회
+
