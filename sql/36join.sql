1fe78787e952699e56cb249ac29615b1e3cc6d08
sebaek <sebaek@gmail.com>
2024-04-26 12:09:41 +0900

    join

--- sql/36join.sql
+++ sql/36join.sql
@@ -25,4 +25,32 @@ FROM Products p
 WHERE p.ProductID = 10;
 
 # 10249번 주문(Orders)을 한 고객의 이름(Customers) 조회
+SELECT c.CustomerName
+FROM Orders o
+         JOIN Customers c
+              ON o.CustomerID = c.CustomerID
+WHERE o.OrderID = 10249;
+
+# 10249번 주문(Orders)을 처리한 직원의 이름(Employees) 조회
+SELECT e.FirstName, e.LastName
+FROM Orders o
+         JOIN Employees e
+              ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderID = 10249;
+# 10249번 주문(Orders)을 배달한 Shipper의 이름(Shippers) 조회
+SELECT s.ShipperName
+FROM Orders o
+         JOIN Shippers s
+              ON o.ShipperID = s.ShipperID
+WHERE OrderID = 10249;
+
+# 90번 고객의 이름과 주문한 날짜들을 오름차순으로 조회
+SELECT c.CustomerName, o.OrderDate
+FROM Orders o
+         JOIN Customers c
+              ON o.CustomerID = c.CustomerID
+WHERE c.CustomerID = 90
+ORDER BY o.OrderDate;
+
+# 3번 직원이 주문을 처리한 날짜들을 오름차순 조회 (직원이름, 날짜)
 
