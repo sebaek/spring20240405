1311488773813ab79518da8a91cfe4515b6fa366
sebaek <sebaek@gmail.com>
2024-04-29 10:39:12 +0900

    서브 쿼리

---
+++ sql/44subquery.sql
@@ -0,0 +1,80 @@
+# 44subquery.sql
+
+USE w3schools;
+
+# 1행1열 결과 (scalar)
+SELECT AVG(Price)
+FROM Products;
+
+SELECT *
+FROM Products
+WHERE Price >= 28.866364;
+
+SELECT *
+FROM Products
+WHERE Price >= (SELECT AVG(Price)
+                FROM Products);
+
+# todo ; 1번 카테고리 상품의 평균가격보다 높은 2번 카테고리 상품명 조회
+SELECT AVG(Price)
+FROM Products;
+SELECT ProductName
+FROM Products
+WHERE Price >= (SELECT AVG(Price)
+                FROM Products)
+  AND CategoryID = 2;
+
+# 여러행 1열 결과
+SELECT CustomerID
+FROM Customers
+WHERE Country = 'mexico';
+
+# mexico 고객이 주문한 건들 조회
+SELECT *
+FROM Orders
+WHERE CustomerID IN (2, 3, 13, 58, 80);
+
+SELECT *
+FROM Orders
+WHERE CustomerID IN (SELECT CustomerID
+                     FROM Customers
+                     WHERE Country = 'mexico');
+
+SELECT *
+FROM Orders o
+         JOIN Customers c
+              ON o.CustomerID = c.CustomerID
+WHERE Country = 'mexico';
+
+# todo ; 1번 카테고리 상품들이 주문된 날짜들 조회
+SELECT ProductID
+FROM Products
+WHERE CategoryID = 1;
+
+SELECT OrderID
+FROM OrderDetails
+WHERE ProductID IN (1, 2, 24);
+SELECT OrderDate
+FROM Orders
+WHERE OrderID IN (10254, 10255);
+
+SELECT OrderDate
+FROM Orders
+WHERE OrderID IN (SELECT OrderID
+                  FROM OrderDetails
+                  WHERE ProductID IN (SELECT ProductID
+                                      FROM Products
+                                      WHERE CategoryID = 1));
+
+SELECT o.OrderDate
+FROM Orders o
+         JOIN OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN Products p
+              ON od.ProductID = p.ProductID
+WHERE p.CategoryID = 1;
+
+
+# 여러행 여러열 결과
+
+
