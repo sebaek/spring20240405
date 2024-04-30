b5c3306751b6722f14fc00525aba6b61a51db148
sebaek <sebaek@gmail.com>
2024-04-30 10:29:18 +0900

    transaction

--- src/main/java/com/study/controller/Controller35.java
+++ src/main/java/com/study/controller/Controller35.java
@@ -22,4 +22,20 @@ public class Controller35 {
     public void sub2() {
         service.transferMoney2();
     }
+
+    @GetMapping("sub3")
+    public void sub3() throws Exception {
+        // checked exception 은 rollback 안됨
+        service.transferMoney3();
+    }
+
+    @GetMapping("sub4")
+    public void sub4() throws Exception {
+        service.transferMoney4();
+    }
+
+    @GetMapping("sub5")
+    public void sub5() {
+        service.transferMoney5();
+    }
 }
--- src/main/java/com/study/service/Service02.java
+++ src/main/java/com/study/service/Service02.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+// service 내 모든 메소드에 적용됨
+@Transactional(rollbackFor = Exception.class)
 public class Service02 {
     private final Mapper06 mapper;
 
@@ -25,6 +27,51 @@ public class Service02 {
         if (true) {
             throw new RuntimeException();
         }
+
+        // 흥민이 돈 500 더하기
+        mapper.plusMoney();
+    }
+
+    @Transactional
+    public void transferMoney3() throws Exception {
+        // 강인이 돈 500 빼기
+        mapper.minusMoney();
+
+        if (true) {
+            throw new Exception();
+        }
+
+        // 흥민이 돈 500 더하기
+        mapper.plusMoney();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void transferMoney4() throws Exception {
+        // 강인이 돈 500 빼기
+        mapper.minusMoney();
+
+        if (true) {
+            throw new Exception();
+        }
+
+        // 흥민이 돈 500 더하기
+        mapper.plusMoney();
+    }
+
+    @Transactional
+    public void transferMoney5() {
+        // 강인이 돈 500 빼기
+        mapper.minusMoney();
+
+
+        if (true) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
         // 흥민이 돈 500 더하기
         mapper.plusMoney();
