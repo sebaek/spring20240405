0eb2063a3f8842aad60d00c8f12caa43d6c4a9e7
sebaek <sebaek@gmail.com>
2024-04-30 16:16:36 +0900

    login

--- src/main/java/com/study/config/SecurityConfiguration.java
+++ src/main/java/com/study/config/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.study.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
--- src/main/java/com/study/controller/Controller38.java
+++ src/main/java/com/study/controller/Controller38.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +9,13 @@ public class Controller38 {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
+    }
+
+    @GetMapping("/path1")
+    @PreAuthorize("isAuthenticated()")
+    // https://docs.spring.io/spring-security/reference/5.8/servlet/authorization/expression-based.html
+    public void path1() {
+        System.out.println("Controller38.path1");
     }
 }
