7c8312f0e86f4808048296b5a06fd61efd17d675
sebaek <sebaek@gmail.com>
2024-05-16 09:35:24 +0900

    /api/someurl 의 get,post 요청핸들러메소드 작성

---
+++ src/main/java/com/study/controller/Controller40.java
@@ -0,0 +1,19 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Controller40 {
+
+    @GetMapping("/api/someurl")
+    public void method1() {
+        System.out.println("Controller40.method1");
+    }
+
+    @PostMapping("/api/someurl")
+    public void method2() {
+        System.out.println("Controller40.method2");
+    }
+}
