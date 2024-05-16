a7f00a089caffa9c2fd8e08f56be508d03461dab
sebaek <sebaek@gmail.com>
2024-05-16 10:14:11 +0900

    /api/someurl3 의 get 요청처리메소드

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -3,6 +3,7 @@ package com.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controller40 {
@@ -27,5 +28,15 @@ public class Controller40 {
     @PostMapping("/api/someurl2")
     public void method4() {
         System.out.println("Controller40.method4");
+    }
+
+    @GetMapping("/api/someurl3")
+    public void method5(@RequestParam("id") Integer id,
+                        @RequestParam("name") String name,
+                        @RequestParam("email") String email) {
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
     }
 }
