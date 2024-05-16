be17230d313f989387998e182beabe6333f3a796
sebaek <sebaek@gmail.com>
2024-05-16 11:23:02 +0900

    @RequestBody:json 파싱해서 map으로 받기

---
+++ src/main/java/com/study/controller/Controller41.java
@@ -0,0 +1,22 @@
+package com.study.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/main41")
+public class Controller41 {
+
+    @PostMapping("sub1")
+    public void sub1(@RequestBody Map<String, Object> map) {
+        // @RequestBody
+        // 역직렬화(파싱, 디코딩) JSON -> Map
+        System.out.println("map = " + map);
+    }
+
+}
