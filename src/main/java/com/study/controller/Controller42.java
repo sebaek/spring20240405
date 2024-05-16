921d0c189a62478e23b7af44e388d06f10b113ba
sebaek <sebaek@gmail.com>
2024-05-16 14:44:53 +0900

    JavaBean 객체 응답
    json으로 직렬화할때
    get메소드로 프로퍼티가 결정됨

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import com.study.domain.MyBean411;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,5 +52,15 @@ public class Controller42 {
                 "foods", List.of("pizza", "coffee"),
                 "desc", Map.of("team", "london", "location", "uk"),
                 "hasAccess", false);
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5() {
+        MyBean411 obj = new MyBean411();
+        obj.setName("이강인");
+        obj.setAge(55);
+
+        return obj; // json으로 직렬화
     }
 }
--- src/main/java/com/study/domain/MyBean411.java
+++ src/main/java/com/study/domain/MyBean411.java
@@ -7,4 +7,8 @@ import lombok.Data;
 public class MyBean411 {
     private String name;
     private Integer age;
+
+    public String getDesc() {
+        return STR."이름은 \{name}이고 나이는 \{age}세 입니다.";
+    }
 }
