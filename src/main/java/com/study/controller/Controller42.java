e2326ca76dff77c0b3725cf254e85fc3637f18b0
sebaek <sebaek@gmail.com>
2024-05-16 14:18:58 +0900

    String 본문 응답

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -15,4 +15,17 @@ public class Controller42 {
 
         return "어떤 데이터";
     }
+
+    @GetMapping("sub2")
+    @ResponseBody
+    public String method2() {
+        double random = Math.random();
+        String result = "";
+        if (random < 1.0 / 3) {
+            return "가위";
+        } else if (random < 2.0 / 3) {
+            return "바위";
+        }
+        return "보";
+    }
 }
