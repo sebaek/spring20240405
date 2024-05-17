13439b87abfbf8ee860cf22273f31692ead62f9c
sebaek <sebaek@gmail.com>
2024-05-17 12:16:38 +0900

    토큰생성시 권한 포함
    권한 별 접근 가능한 메소드 작성

--- src/main/java/com/study/controller/Controller44.java
+++ src/main/java/com/study/controller/Controller44.java
@@ -24,14 +24,14 @@ public class Controller44 {
         // 사용자 이름
         String username = map.get("username").toString();
         // 권한
-//        map.get("scope");
+        String scope = map.get("scope").toString();
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(Instant.now())
                 .expiresAt(Instant.now().plusSeconds(60 * 60 * 24))
                 .subject(username)
-//                .claim("scope", "") // 권한
+                .claim("scope", scope) // 권한
                 .build();
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
@@ -47,6 +47,27 @@ public class Controller44 {
     @PreAuthorize("isAuthenticated()")
     public String user() {
         return "로그인 한 유저만 접근 가능한 경로";
+    }
+
+    @GetMapping("admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public String admin() {
+        return "어드민만 접근 가능한 경로";
+    }
+
+    @GetMapping("manager")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_manager')")
+    public String manager() {
+        return "매니저만 접근 가능한 경로";
+    }
+
+    @GetMapping("ma")
+    @ResponseBody
+    @PreAuthorize("hasAnyAuthority('SCOPE_manager', 'SCOPE_admin')")
+    public String ma() {
+        return "매니져 또는 어드민이 접근 가능한 경로";
     }
 
 }
